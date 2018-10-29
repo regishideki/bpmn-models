df8d208ada107e83153f324f3143f7245ef2501c
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2018-10-29 17:36:06 -0200

    extract bpmn file in deploy

--- deploy_workflow.sh
+++ deploy_workflow.sh
@@ -2,9 +2,10 @@
 
 CAMUNDA_URL=http://localhost:8080
 DEPLOYMENT_NAME='My Deployment'
+BPMN=@src/main/resources/auto_refi/growth_process/201810291438.bpmn
 
  curl -X POST \
     $CAMUNDA_URL/engine-rest/deployment/create \
     -H 'content-type: multipart/form-data; boundary=----a0b7961d-7e05-4f16-be5e-badafbe91233' \
     -F deployment-name=$DEPLOYMENT_NAME \
-    -F bpmn=@src/main/resources/growthProcess.bpmn
+    -F bpmn=$BPMN
